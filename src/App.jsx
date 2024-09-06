c32b68c56688595e56a0eda2e7f3847c3e91dbd3
carlos.aquino <carlos.aquino@coppel.com>
2024-09-06 18:43:18 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import ColorSwitch from './components/ColorSwitch'
+
+function App() {
+  return (
+    <>
+    <ColorSwitch/>
+    </>
+  )
+}
+
+export default App
